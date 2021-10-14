79aea3950e3d6f74d902279e276a98d9cf48292a
KaterinTello <u.kate.tello@gmail.com>
2021-10-13 23:57:12 -0500

    agregando los cambios hechos hasta ahora en html,css y js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 import validator from './validator.js';
 
-console.log(validator);
+const validation_button = document.getElementById("validation_button");
+const overlay = document.getElementById("overlay");
+const cerrar_con_btn_ok = document.getElementById("cerrar_con_btn_ok");
+// let  cvv = document.getElementById("cvv");
+// let  mm = document.getElementById("mm");
+// let  nombre_usuario = document.getElementById("nombre_usuario");
+
+validation_button.addEventListener("click", e => {
+  e.preventDefault
+
+  // ---Activar el popup---
+  overlay.classList.add("active");
+
+  //--- evaluar magnitud de palabra nombre_usuario--
+  // if(nombre_usuario.value.length < 6){
+
+  //   alert("Nombre muy pequeño");
+  
+  // }
+  // -----Validando tarjeta ------
+
+  let  creditCardNumber = document.getElementById("numero_tarjeta");
+  let  resultado = validator.isValid(creditCardNumber.value);
+  let ocultar_datos=validator.maskify(creditCardNumber.value);
+  
+  if (resultado === true){
+
+    document.getElementById("validacion").innerHTML="Gracias validado! "+ocultar_datos;
+  }else{
+
+    document.getElementById("validacion").innerHTML="invalido! "+ocultar_datos;
+  }
+
+})
+
+cerrar_con_btn_ok.addEventListener("click", e => {
+  e.preventDefault
+   overlay.classList.remove("active");
+  
+})
+// console.log(validator);
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,45 @@
 const validator = {
   // ...
+
+  isValid: (creditCardNumber) => {
+
+    let suma=0;
+     //Array del numero de la tarjeta en orden invertido
+    let arrayNumTarjeta= creditCardNumber.toString().split("").reverse();
+     //recorrido por cada elemento del array invertido
+    for (let i = 0; i < arrayNumTarjeta.length;i++){
+      //añadiendo condicional para posicion par 
+        if ( i % 2 != 0) {
+            arrayNumTarjeta[i] *= 2;
+            if (arrayNumTarjeta[i] >= 10){
+                arrayNumTarjeta[i] = (arrayNumTarjeta[i] % 10) + (Math.floor(arrayNumTarjeta[i] / 10));
+            }
+        }      
+        suma += parseInt(arrayNumTarjeta[i]);  
+    }
+     //añadiendo condicional que indicará si es o no válida la tarjeta
+    if (suma%10 == 0) {
+        return true;
+    }else {
+        return false;
+    }  // prueba (1234567890) --devuelve false--
+    
+  }
+  //ocultando caracteres de la tarjeta 
+  , maskify: (creditCardNumber) => {
+
+      let resultados ="";
+      for(let i=0; i<creditCardNumber.length; i++ ){
+          if(i<=creditCardNumber.length-5){
+              resultados+="#";
+          }else {
+              resultados+=creditCardNumber[i];
+          }
+      }
+    
+      return resultados;
+  }
+
 };
 
 export default validator;
